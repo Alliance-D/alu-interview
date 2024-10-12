cd863630a072f4a93f0c9ec3263905b8b9983fdd
Alliance-D <a.dushimezi@alustudent.com>
2024-10-12 12:12:51 +0000

    0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,7 @@
 
 """returns a list of lists of integers representing the Pascal's triangle"""
 
+
 def pascal_triangle(n):
     """Returns a list of lists representing Pascal's triangle of n rows."""
     if n <= 0:
