a1ed4f21d683bd3a35f8241ff2ae289fdda9a1c1
Alliance-D <a.dushimezi@alustudent.com>
2024-10-12 12:10:49 +0000

    0-pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
-import math
-
 """returns a list of lists of integers representing the Pascal's triangle"""
 
 def pascal_triangle(n):
@@ -9,10 +7,13 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     
-    triangle = []
+    triangle = [[1]]
     
-    for i in range(n):
-        row = [math.comb(i, j) for j in range(i + 1)]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
         triangle.append(row)
     
     return triangle
